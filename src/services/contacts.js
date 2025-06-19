eecadb376722ac3a379e89c1e360797471c0b998
Sergii Klimchuk <v.buh911@gmail.com>
2025-06-19 09:32:57 +0300

    fixed services

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,6 +1,6 @@
-import { Contact } from '../db/models/contactModel.js';
+import { Contact } from '../db/models/contactModel.js'; // Імпортуємо модель
 
-// Отримати всі контакти з пагінацією і сортуванням
+// Отримати всі контакти з пагінацією та сортуванням
 export const getAllContacts = async ({
   page = 1,
   perPage = 10,
@@ -10,23 +10,18 @@ export const getAllContacts = async ({
   const skip = (page - 1) * perPage;
   const sortDirection = sortOrder === 'desc' ? -1 : 1;
 
-  const sortOption = {};
-  if (sortBy) {
-    sortOption[sortBy] = sortDirection;
-  }
-
   const totalItems = await Contact.countDocuments();
+  const totalPages = Math.ceil(totalItems / perPage);
+
   const contacts = await Contact.find()
-    .sort(sortOption)
+    .sort({ [sortBy]: sortDirection })
     .skip(skip)
     .limit(perPage);
 
-  const totalPages = Math.ceil(totalItems / perPage);
-
   return {
     data: contacts,
-    page,
-    perPage,
+    page: Number(page),
+    perPage: Number(perPage),
     totalItems,
     totalPages,
     hasPreviousPage: page > 1,
@@ -38,30 +33,22 @@ export const getAllContacts = async ({
 export const getContactById = async (id) => {
   const contact = await Contact.findById(id);
   return contact;
-
 };
 
-  };
-
-
+// Створити новий контакт
 export const createContact = async (data) => {
   return Contact.create(data);
 };
 
+// Оновити контакт за _id
 export const patchContactById = async (id, data) => {
   return Contact.findByIdAndUpdate(id, data, {
     new: true, // повертає оновлений обʼєкт
     runValidators: true, // перевіряє типи/схему
   });
-
 };
 
-export const deleteContactById = async (id) => {
-  return Contact.findByIdAndDelete(id);
-};
-=======
-  };
-  
+// Видалити контакт за _id
 export const deleteContactById = async (id) => {
   return Contact.findByIdAndDelete(id);
 };
