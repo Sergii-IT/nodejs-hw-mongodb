061d620f8159d9e1a5ea39c3c2ca3911b8248bbf
Sergii Klimchuk <v.buh911@gmail.com>
2025-07-25 14:31:50 +0300

    Hw7 swagger (#21)
    * done
    
    * fixed1
    
    * fixed2
    
    * fixed3
    
    * fixed8
    
    * fixed9

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -7,28 +7,28 @@ import {
   handleSendResetEmail,
   resetPassword, 
 } from '../controllers/auth.js';
-
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from '../middlewares/validateBody.js';
 import { registerSchema, loginSchema, emailSchema, resetPasswordSchema } from '../schemas/authSchemas.js';
 
 const router = express.Router();
 
 // POST /auth/register
-router.post('/register', validateBody(registerSchema), registerUser);
+router.post('/register', validateBody(registerSchema), ctrlWrapper(registerUser));
 
 // POST /auth/login
-router.post('/login', validateBody(loginSchema), loginUser);
+router.post('/login', validateBody(loginSchema), ctrlWrapper(loginUser));
 
 // POST /auth/refresh
-router.post('/refresh', refreshSession);
+router.post('/refresh', ctrlWrapper(refreshSession));
 
 // POST /auth/logout
-router.post('/logout', logoutUser);
+router.post('/logout', ctrlWrapper(logoutUser));
 
 // POST /auth/send-reset-email
-router.post('/send-reset-email', validateBody(emailSchema), handleSendResetEmail);
+router.post('/send-reset-email', validateBody(emailSchema), ctrlWrapper(handleSendResetEmail));
 
 // POST /auth/reset-pwd
-router.post('/reset-pwd', validateBody(resetPasswordSchema), resetPassword);
+router.post('/reset-pwd', validateBody(resetPasswordSchema), ctrlWrapper(resetPassword));
 
 export default router;
